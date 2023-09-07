0042cac90985bec1afbf63fff71dbfc7d9935e43
Amal Joseph Varghese <amaljova@gmail.com>
2023-09-07 16:22:12 +0530

    metadta collection has the option of "history"now.

--- src/QuickDicom/metadata/collectData.py
+++ src/QuickDicom/metadata/collectData.py
@@ -12,10 +12,19 @@ from tqdm import tqdm
 
 
 
-def getData(base: Path) -> list:
-    '''Give a path, this function will return list of DICOM files metadata'''
+def getPathsToScan(base:Path, history = []) -> set:
+    '''Get all the new files'''
+    present = set(Path(base).rglob("*"))
+    history = set([Path(f_path) for f_path in history])
+    return  present - history
+
+
+def getData(base: Path, history = []) -> list:
+    '''Give a path, this function will return list of DICOM files metadata
+    (Not all metadata, but whatever have specified in the dicomInfo.py)'''
     data_list = []
-    for file in tqdm(Path(base).rglob("*")):
+    # for file in tqdm(Path(base).rglob("*")):
+    for file in tqdm(getPathsToScan(base, history)):
         if file.is_file():
             try:
                 data_list.append(dcmFile(file).getAllInfo())
